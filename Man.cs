16714e053d4f471a43b224b1ca955c2754150019
Michail95 <44161522+Michail95@users.noreply.github.com>
2019-03-18 12:05:09 +0300

    Add files via upload

---
+++ Man.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D1
+{
+   public class Man
+    {
+        public string name { get; set; }
+        public string pol { get; set; }
+        public int tail { get; set; }
+        public int mas { get; set; }
+        public string birth { get; set; }
+
+        public Man(string _n, string _p, int _t, int _m, string _b)
+        {
+            name = _n;
+            pol = _p;
+            tail = _t;
+            mas = _m;
+            birth = _b;
+        }
+    }
+}
